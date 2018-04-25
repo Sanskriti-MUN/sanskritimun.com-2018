acb70fb7f4f3331c5ad123cb7983064596811eb1
Mihir Chaturvedi <plibither8@gmail.com>
2018-04-25 14:53:28 +0530

    Remove countdown from homepage

--- dist/assets/js/scripts.js
+++ dist/assets/js/scripts.js
@@ -1,4 +1,4 @@
-// Change color of navbar onscroll	+if(change_color){var header=document.querySelector("nav"),orange="#ffa500f7";header.style.background="transparent",window.addEventListener("scroll",function(){window.scrollY>=200?header.style.background=orange:header.style.background="transparent"})}var timer,end=new Date("04/25/2018 0:0 AM"),_second=1e3,_minute=60*_second,_hour=60*_minute,_day=24*_hour;function showRemaining(){var e=new Date,n=ende;if(n<0)return clearInterval(timer),void(document.getElementById("countdown").innerHTML="EXPIRED!");var r=Math.floor(n/_day),o=Math.floor(n%_day/_hour),t=Math.floor(n%_hour/_minute),a=Math.floor(n%_minute/_second);document.querySelector(".days p").innerHTML=r,document.querySelector(".hours p").innerHTML=o,document.querySelector(".minutes p").innerHTML=t,document.querySelector(".seconds p").innerHTML=a}timer=setInterval(showRemaining,1e3); 
+// Change color of navbar onscroll
 if (change_color) {
     var header = document.querySelector('nav');
     var orange = "#ffa500f7";
@@ -16,6 +16,7 @@ if (change_color) {
     
     
 // Countdown	
+/*
 var end = new Date('04/25/2018 9:0 AM');
 var _second = 1000;
 var _minute = _second * 60;
@@ -43,4 +44,4 @@ function showRemaining() {
     document.querySelector('.seconds p').innerHTML = seconds;
 }
 
-timer = setInterval(showRemaining, 1000); 
+timer = setInterval(showRemaining, 1000); */
